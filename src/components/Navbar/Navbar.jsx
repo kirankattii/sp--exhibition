4973be2204eab8a58e5546a7f369214cce043ed6
kirankattii <kirankatti550@gmail.com>
2025-06-22 08:24:52 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,45 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import { assets } from "../../assets/assets";
+// import styles from "./navbar.module.css";
+// import { MediaNav2 } from "./mediaNav2";
+
+// const Navbar = () => {
+//   return (
+//     <div className={styles.container}>
+//       <nav>
+//         <img src={assets.new_logo2} alt="" />
+//         <ul>
+//           <li>
+//             <Link to={"/"}>HOME</Link>
+//           </li>
+//           <li>
+//             <Link to={"/about-us"}>ABOUT</Link>
+//           </li>
+//           <li>
+//             <Link to={"/clients"}>CLIENT PROJECT</Link>
+//           </li>
+//           <li>
+//             <Link to={"/gallary"}>GALLERY</Link>
+//           </li>
+
+//           <li>
+//             <Link to={"/contact"}>CONTACT</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <div className={styles.mediaNav}>
+//         <img src={assets.new_logo2} alt="" />
+//         <MediaNav2 />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+import { NavLink } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import styles from "./navbar.module.css";
 import { MediaNav2 } from "./mediaNav2";
@@ -10,20 +51,19 @@ const Navbar = () => {
         <img src={assets.new_logo2} alt="" />
         <ul>
           <li>
-            <Link to={"/"}>HOME</Link>
+            <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ""}>HOME</NavLink>
           </li>
           <li>
-            <Link to={"/about-us"}>ABOUT</Link>
+            <NavLink to="/about-us" className={({ isActive }) => isActive ? styles.active : ""}>ABOUT</NavLink>
           </li>
           <li>
-            <Link to={"/clients"}>CLIENT PROJECT</Link>
+            <NavLink to="/clients" className={({ isActive }) => isActive ? styles.active : ""}>CLIENT PROJECT</NavLink>
           </li>
           <li>
-            <Link to={"/gallary"}>GALLERY</Link>
+            <NavLink to="/gallary" className={({ isActive }) => isActive ? styles.active : ""}>GALLERY</NavLink>
           </li>
-
           <li>
-            <Link to={"/contact"}>CONTACT</Link>
+            <NavLink to="/contact" className={({ isActive }) => isActive ? styles.active : ""}>CONTACT</NavLink>
           </li>
         </ul>
       </nav>
